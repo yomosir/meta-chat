1674de142edd4eb18165e0dae57ebbd302471d3f
zhengguochun <zheng13239@163.com>
2022-02-19 22:38:17 +0800

    添加ios端项目源码

---
+++ meta-chat-ios/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 郑国春 on 2022/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ meta-chat-ios/Shared/meta_chat_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  meta_chat_iosApp.swift
+//  Shared
+//
+//  Created by 郑国春 on 2022/2/19.
+//
+
+import SwiftUI
+
+@main
+struct meta_chat_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
