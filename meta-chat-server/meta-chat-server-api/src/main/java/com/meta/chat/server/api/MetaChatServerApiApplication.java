bf1ef52b0760916f8a269d5e30532dedb26bf41e
zhengguochun <zheng13239@163.com>
2022-02-22 22:39:27 +0800

    添加server项目，api层

---
+++ meta-chat-server/meta-chat-server-api/src/main/java/com/meta/chat/server/api/MetaChatServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.meta.chat.server.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaChatServerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaChatServerApiApplication.class, args);
+    }
+
+}
