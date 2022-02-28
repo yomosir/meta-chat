1e147a104804594184beb4793ec8c370e55b8cfd
zhengguochun <zheng13239@163.com>
2022-02-28 22:49:20 +0800

    添加练习测试代码

--- meta-chat-ios/Shared/ContentView.swift
+++ meta-chat-ios/Shared/ContentView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading){
+            Text("MetaChat")
+                .font(.title)
+                .foregroundColor(.green)
+            HStack{
+                Text("a simple chat tool")
+                    .font(.subheadline)
+                Spacer()
+                Text("hangzhou")
+                    .font(.subheadline)
+            }
+        }.padding()
+            
     }
 }
 
