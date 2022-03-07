0bf4f4efc502ed93a8daec8b249065ab53c4c963
zhengguochun <zheng13239@163.com>
2022-03-07 23:02:46 +0800

    组合之前的例子

--- meta-chat-ios/Shared/ContentView.swift
+++ meta-chat-ios/Shared/ContentView.swift
@@ -9,18 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading){
-            Text("MetaChat")
-                .font(.title)
-                .foregroundColor(.green)
-            HStack{
-                Text("a simple chat tool")
-                    .font(.subheadline)
-                Spacer()
-                Text("hangzhou")
-                    .font(.subheadline)
-            }
-        }.padding()
+    
+        VStack(){
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading){
+                
+                Text("MetaChat")
+                    .font(.title)
+                    .foregroundColor(.green)
+                HStack{
+                    Text("a simple chat tool")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("hangzhou")
+                        .font(.subheadline)
+                }
+                Divider()
+                Text("About Turtle Rock")
+                                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }.padding()
+            
+            Spacer()
+        }
             
     }
 }
