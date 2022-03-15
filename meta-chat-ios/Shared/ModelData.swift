acfc44311d7d90e3ba6922a7de6383efbb4705f4
zhengguochun <zheng13239@163.com>
2022-03-15 20:18:09 +0800

    添加逻辑注释

--- meta-chat-ios/Shared/ModelData.swift
+++ meta-chat-ios/Shared/ModelData.swift
@@ -9,6 +9,8 @@ import Foundation
 
 var landmarks: [Landmark] = load("landmarkData.jsn")
 
+//声明一个方法，加载landmarkData.json中的数据
+//_（表示不是用别名） filename: String，返回一个Decodable对象
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
@@ -16,13 +18,13 @@ func load<T: Decodable>(_ filename: String) -> T {
     else{
         fatalError("Couldn't find \(filename) in main bundle. ")
     }
-    
+    // 读取数据到data中
     do {
         data = try Data(contentsOf: file)
     }catch{
         fatalError("Couldn't load \(filename) from main bundle: \n \(error)")
     }
-    
+    // 进行decode
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
