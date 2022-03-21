f9b538eb957d89579bd071f27348cf13be9bf9f2
zhengguochun <zheng13239@163.com>
2022-03-21 22:43:15 +0800

    添加用户详情页面UI文件

---
+++ meta-chat-ios/Shared/UserProfile.swift
@@ -0,0 +1,19 @@
+//
+//  用户详情界面，展示用户相关数据
+//
+//  Created by 郑国春 on 2022/3/21.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
