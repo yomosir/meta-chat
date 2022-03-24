40e8c1b9d75b882e98e5d0ad8750bb767cbc16fb
zhengguochun <zheng13239@163.com>
2022-03-24 23:32:16 +0800

    添加主页面文件

---
+++ meta-chat-ios/Shared/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  meta-chat-ios
+//
+//  Created by 郑国春 on 2022/3/24.
+//
+
+import SwiftUI
+
+struct main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct main_Previews: PreviewProvider {
+    static var previews: some View {
+        main()
+    }
+}
