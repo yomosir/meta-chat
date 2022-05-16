d57d0977b077588dd9801139389c53267d3af0f0
zhengguochun <zheng13239@163.com>
2022-05-16 21:01:39 +0800

    add .

--- meta-chat-server/meta-chat-server-infrastructure/src/main/java/com/meta/chat/server/common/BizCode.java
+++ meta-chat-server/meta-chat-server-infrastructure/src/main/java/com/meta/chat/server/common/BizCode.java
@@ -5,4 +5,6 @@ public class BizCode {
     public final static String BIZ_ONE = "ali.cola.demo.bizOne"; //biz one
 
     public final static String BIZ_TWO = "ali.cola.demo.bizTwo"; //biz two
+
+    ThreadLocal
 }
