ac41810b12f6c327a1c2386d58b9a083a72d22c5
zhengguochun <zheng13239@163.com>
2022-05-21 23:52:22 +0800

    add .

--- meta-chat-server/meta-chat-server-domain/src/main/java/com/meta/chat/server/domain/DomainFactory.java
+++ meta-chat-server/meta-chat-server-domain/src/main/java/com/meta/chat/server/domain/DomainFactory.java
@@ -2,10 +2,12 @@ package com.meta.chat.server.domain;
 
 import com.meta.chat.server.domain.user.UserProfile;
 
+import java.util.Calendar;
+
 public class DomainFactory {
 
     public static UserProfile getUserProfile(){
         return new UserProfile();
     }
-
+    Calendar
 }
