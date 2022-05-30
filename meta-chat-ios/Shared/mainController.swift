91a9d1077114ecc34305a75e2b843b5cd881197b
zhengguochun <zheng13239@163.com>
2022-05-30 23:43:26 +0800

    add

--- meta-chat-ios/Shared/mainController.swift
+++ meta-chat-ios/Shared/mainController.swift
@@ -3,7 +3,8 @@
 //  meta-chat-ios
 // 主页面的逻辑文件,提交,test,凑数,11111,1111,llll,2222,333，444
 // aaaa，bbb,aaa，ccccc，ssss，，aaaaaaaa,aaaa,aaaa,fasdfa，assa，aaa
-// aaa 43361800, sssss，aaaaa，aaaa，asasaasa，aaaa,aaa
+// aaa 43361800, sssss，aaaaa，aaaa，asasaasa，aaaa,aaa,aaaa
+
 
 
 //  Created by 郑国春 on 2022/3/27.
